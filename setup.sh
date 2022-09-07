f55668668bef9f855a6d5c9d5765c80f93a28817
Kyle Derby MacInnis <director@sovereign.enterprises>
2022-09-06 21:12:24 -0600

    add babel rc

--- setup.sh
+++
@@ -1,6 +0,0 @@
-tsc --outDir pixos/jsx/engine/utils/obj pixos/jsx/engine/utils/obj/ts/* --downlevelIteration
-
-npm install webpack webpack-cli webpack-dev-server --save-dev
-npm install @babel/cli @babel/core @babel/node @babel/preset-env @babel/preset-react @babel/register babel-register --save-dev
-npm install babel-loader style-loader css-loader --legacy-peer-deps
-npx webpack
