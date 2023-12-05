bcd470b99caf33df66a532925eb2eeae7402cc5d
Kyle Derby MacInnis <director@sovereign.enterprises>
2023-12-05 12:44:46 -0700

    minor

--- example/src/App.js
+++ example/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Pixos from 'calliope-pixos';
-const PixosAvatar = Pixos['calliope-pixos'].default;
+const PixosClient = Pixos['calliope-pixos'].default;
 function App() {
   return (
     <div className="App" width="650px">
-      <PixosAvatar />
+      <PixosClient />
     </div>
   );
 }
