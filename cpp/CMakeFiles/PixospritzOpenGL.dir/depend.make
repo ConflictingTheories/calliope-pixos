a596ff850f9f73108fcb7d5b01e666533bad30d4
Kyle Derby MacInnis <kderbyma@gmail.com>
2025-11-22 00:15:44 -0800

    clean

--- cpp/CMakeFiles/PixospritzOpenGL.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for PixospritzOpenGL.
-# This may be replaced when dependencies are built.
--- cpp/include/stb_image.h
+++
@@ -1,6 +0,0 @@
-// stb_image - public domain single-file header
-// Minimal wrapper: include stb_image implementation here
-#ifndef STB_IMAGE_IMPLEMENTATION
-#define STB_IMAGE_IMPLEMENTATION
-#endif
-#include "../third_party/stb_image.h"
